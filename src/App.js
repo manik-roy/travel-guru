f3a40187dc22a45d3961d0ee7ff139c569a836ef
manik-roy <cm.dpi15@gmail.com>
2020-10-02 16:20:20 +0600

    add context api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom'
 import Login from './components/auth/Login';
 import Booking from './components/booking/Booking';
@@ -6,21 +6,24 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Search from './components/search/Search';
 
+export const UserContext = createContext();
 function App() {
   const location = useLocation();
-  console.log(location.pathname);
+  const [user, setUser] = useState(null);
+  const [bookingInfo, setBookingInfo] = useState({});
+  
   return (
-
-    <div className={`${location.pathname === '/' || location.pathname.includes('booking') ? "home" : ""}`}>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/booking/:id" component={Booking} />
-        <Route path="/search/:id" component={Search} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </div>
-
+    <UserContext.Provider value={{ user, setUser, bookingInfo, setBookingInfo }}>
+      <div className={`${location.pathname === '/' || location.pathname.includes('booking') ? "home" : ""}`}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/booking/:id" component={Booking} />
+          <Route path="/search/:id" component={Search} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </UserContext.Provider>
   );
 }
 
